0cbe6537b30869ad2ba94910d0c6315c857f23e4
sumin-cheon <s2minni@naver.com>
2021-01-20 17:12:44 +0900

    1차 세팅

---
+++ NCS/js/sumin.js
@@ -0,0 +1,25 @@
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+
+
+});
